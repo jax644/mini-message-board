dace2f1810ed1245d14d1376a73b6175a0222d02
Jacqueline Taylor <jacquelinetaylor644@gmail.com>
2024-11-27 14:21:11 -0800

    Add new message page

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
+// Initialize server
 const express = require('express')
 const app = express()
 
+// ???
+const path = require('path')
+
+// Set up app to get URL encoded data from our form
+app.use(express.urlencoded({ extended: true }));
+
+// Set view engine
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+// Import routers
+const indexRouter = require('./routes/indexRouter')
+const newRouter = require('./routes/newRouter')
+
+// Set up route handlers
+app.use('/', indexRouter)
+app.use('/new', newRouter)
+
+// Set up server to listen at a port
 const PORT = 3000
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server running on port ${PORT}`)
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -19,4 +19,11 @@ indexRouter.get('/', (req,res) => {
     res.render('index', { title: 'Mini message board', messages: messages })
 })
 
+indexRouter.post('/new', (req,res) => {
+    const messageUser = req.body.user
+    const messageText = req.body.text
+    messages.push({ text: messageText, user: messageUser, added: new Date() });
+    res.redirect('/')
+})
+
 module.exports = indexRouter
