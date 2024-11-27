d5c4e4601f9121c3288afbfc273b57bbbce44fde
Jacqueline Taylor <jacquelinetaylor644@gmail.com>
2024-11-27 15:08:18 -0800

    Add message page where users can view all details about a message

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -19,11 +19,22 @@ indexRouter.get('/', (req,res) => {
     res.render('index', { title: 'Mini message board', messages: messages })
 })
 
+indexRouter.get('/message/:index', (req,res) => {
+    const message = messages[req.params.index]
+    if (message) {
+        res.render('message', {title: 'Message Details', message:message})
+    } else {
+        res.status(404).send('Message not found')
+    }
+})
+
 indexRouter.post('/new', (req,res) => {
     const messageUser = req.body.user
     const messageText = req.body.text
     messages.push({ text: messageText, user: messageUser, added: new Date() });
     res.redirect('/')
 })
+
+
 
 module.exports = indexRouter
